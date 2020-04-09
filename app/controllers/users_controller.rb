66a2a29ea0bfe4c980ca3500101d8cccd6e9e412
tahirbhalli <tahirbhalli16@gmail.com>
2020-04-09 14:03:46 +0500

    complete project

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,14 @@ class UsersController < ApplicationController
     def index
         @name = "I am the Index action!"
     end
+    def new
+        render 'new user page'
+    end
+    def edit
+        render 'new edit page'
+    end
+    def show
+        render 'show user page'
+    end
+    
 end
--- app/script/rest_requestor.rb
+++ app/script/rest_requestor.rb
@@ -1,5 +1,8 @@
   # app/script/rest_requestor.rb
   require 'rest-client'
-  url = "http://localhost:3000/users"
+  urls=['http://localhost:3000/users','http://localhost:3000/users#edit',
+  'http://localhost:3000/users#show']
+  urls.each do |url|
 
   puts RestClient.get(url)
+  end
